f43697297ce78927a9611f5a2762007f6a45c136
tceppini <tcheppini@gmail.com>
2017-06-28 09:51:22 +0200

    Enregistrement chat
    + Reporting message

---
+++ database/migrations/2017_06_25_093441_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id')->references('id')->on('users');
+            $table->integer('user_id')->references('id')->on('games');
+            $table->text('message');
+            $table->timestamps();
+            $table->create();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
---
+++ database/migrations/2017_06_25_101000_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('message_id')->references('id')->on('messages');
+            $table->integer('user_id')->references('id')->on('games');
+            $table->timestamps();
+            $table->create();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
